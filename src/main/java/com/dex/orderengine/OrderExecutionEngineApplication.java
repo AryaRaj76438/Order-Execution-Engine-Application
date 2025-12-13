f441cad672cf4e2e4c9d4e58cce89782de5e2397
Rajneesh Kumar <192125aryaraj@gmail.com>
2025-12-13 10:14:20 +0530

    Initial Files

---
+++ src/main/java/com/dex/orderengine/OrderExecutionEngineApplication.java
@@ -0,0 +1,13 @@
+package com.dex.orderengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderExecutionEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderExecutionEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dex/orderengine/OrderExecutionEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dex.orderengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderExecutionEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
