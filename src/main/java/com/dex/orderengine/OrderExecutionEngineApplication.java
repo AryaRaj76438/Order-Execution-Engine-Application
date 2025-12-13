896f3087268d800d9bde30a99af1d606af76f969
Rajneesh Kumar <192125aryaraj@gmail.com>
2025-12-13 10:25:41 +0530

    Connection to Database

--- src/main/java/com/dex/orderengine/OrderExecutionEngineApplication.java
+++ src/main/java/com/dex/orderengine/OrderExecutionEngineApplication.java
@@ -1,5 +1,6 @@
 package com.dex.orderengine;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OrderExecutionEngineApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("PGHOST", dotenv.get("PGHOST"));
+		System.setProperty("PGDATABASE", dotenv.get("PGDATABASE"));
+		System.setProperty("PGUSER", dotenv.get("PGUSER"));
+		System.setProperty("PGPASSWORD", dotenv.get("PGPASSWORD"));
 		SpringApplication.run(OrderExecutionEngineApplication.class, args);
 	}
 
